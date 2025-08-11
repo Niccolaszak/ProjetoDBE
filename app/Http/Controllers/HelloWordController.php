44417448bc4714e26dcf47b98da1c5748d454ea2
Aline Avila Brunetti <alinebrunetti88@gmail.com>
2025-08-11 17:49:12 -0300

    Criação da Rota Simples solicitada
    php artisan make:controller HelloWordController
    
    No arquivo web.php:
    use App\Http\Controllers\HelloWordController;
    Route::get('/hello-word',[HelloWordController::class, 'exibirMensagem']);
    
    e no arquivo HelloWordController.php:
    
    public function exibirMensagem(){
            return "Olá Prof! Esta é a rota HelloWorld que solicitou! <br><h3>Solicitação:</h3>Primeira Rota e Controller Simples: Uma rota básica e um controller
    correspondente que retorne um \"Hello World\" ou uma lista simples de
    dados em memória, apenas para demonstrar a configuração inicial do
    Laravel.";
        }

---
+++ app/Http/Controllers/HelloWordController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HelloWordController extends Controller
+{
+    public function exibirMensagem(){
+        return "Olá Prof! Esta é a rota HelloWorld que solicitou! <br><h3>Solicitação:</h3>Primeira Rota e Controller Simples: Uma rota básica e um controller
+correspondente que retorne um \"Hello World\" ou uma lista simples de
+dados em memória, apenas para demonstrar a configuração inicial do
+Laravel.";
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HelloWordController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/hello-word',[HelloWordController::class, 'exibirMensagem']);
+
