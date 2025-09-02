139db721c8a5bd981d81e2a7f9ff5af118229d3f
Aline Avila Brunetti <alinebrunetti88@gmail.com>
2025-09-02 17:34:53 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HelloWorldController;
 use App\Http\Middleware\ForcarRedefinirSenha;
+use App\Http\Controllers\SenhaController;
 
 Route::get('/', function () {
     return view('welcome');
