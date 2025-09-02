d8a543aa22ecfbfee58f0e8ccc990c458b3816e2
Aline Avila Brunetti <alinebrunetti88@gmail.com>
2025-09-02 15:26:32 -0300

    Revert "Alterações - Registro"
    This reverts commit 242797c1bbeaa50c88d0d615d704f0c3717a0e54.

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,23 +4,9 @@
 
         <!-- Name -->
         <div>
-            <x-input-label for="name" :value="__('Nome Completo')" />
+            <x-input-label for="name" :value="__('Name')" />
             <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
             <x-input-error :messages="$errors->get('name')" class="mt-2" />
-        </div>
-
-        <!-- Username -->
-        <div class="mt-4">
-            <x-input-label for="username" :value="__('Seu login será:')" />
-            <x-text-input 
-                id="username" 
-                class="block mt-1 w-full" 
-                type="text" 
-                name="username" 
-                :value="old('username')" 
-                readonly
-            />
-            <x-input-error :messages="$errors->get('username')" class="mt-2" />
         </div>
 
         <!-- Email Address -->
@@ -30,21 +16,9 @@
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
-        <!-- Select Cargo -->
-        <div class="mt-4">
-            <x-input-label for="cargo" :value="__('Cargo')" />
-            <select name="cargo" id="cargo" class="block mt-1 w-full border-gray-300 rounded-md shadow-sm" required>
-                <option value="" disabled selected>Selecione um cargo</option>
-                <option value="admin">Admin</option>
-                <option value="manager">Manager</option>
-                <option value="user">User</option>
-            </select>
-            <x-input-error :messages="$errors->get('cargo')" class="mt-2" />
-        </div>
-
         <!-- Password -->
         <div class="mt-4">
-            <x-input-label for="password" :value="__('Senha')" />
+            <x-input-label for="password" :value="__('Password')" />
 
             <x-text-input id="password" class="block mt-1 w-full"
                             type="password"
@@ -56,7 +30,7 @@
 
         <!-- Confirm Password -->
         <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirmar Senha')" />
+            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
 
             <x-text-input id="password_confirmation" class="block mt-1 w-full"
                             type="password"
@@ -75,20 +49,4 @@
             </x-primary-button>
         </div>
     </form>
-    <script>
-        const nameInput = document.getElementById('name');
-        const usernameInput = document.getElementById('username');
-
-        nameInput.addEventListener('input', () => {
-            let value = nameInput.value.trim();
-            if(value) {
-                let parts = value.split(' ');
-                let first = parts[0].toLowerCase();
-                let last = parts[parts.length - 1].toLowerCase();
-                usernameInput.value = first + '.' + last;
-            } else {
-                usernameInput.value = '';
-            }
-        });
-    </script>
 </x-guest-layout>
