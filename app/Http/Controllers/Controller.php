70ad7765e2578da473fddca66be922dc493958f9
Aline Avila Brunetti <alinebrunetti88@gmail.com>
2025-11-15 18:06:27 -0300

    Update Controller.php

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,15 @@
 
 namespace App\Http\Controllers;
 
-abstract class Controller
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+
+class Controller extends BaseController
 {
-    //
+    /**
+     * AuthorizesRequests: Adiciona o 'authorizeResource' e 'Gate::authorize'
+     * ValidatesRequests: Adiciona o '$request->validate()'
+     */
+    use AuthorizesRequests, ValidatesRequests;
 }
